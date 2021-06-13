df047773e132e9de2a8eaafea01f5274b9d602ff
Hagino T <marspeoplehg@gmail.com>
2021-06-13 18:01:52 +0900

    first commit

---
+++ app/controllers/api/auth/registrations_controller.rb
@@ -0,0 +1,13 @@
+module Api
+  module Auth
+    class RegistrationsController < DeviseTokenAuth::RegistrationsController
+      private
+      def sign_up_params
+        params.permit(:name, :nickname, :email, :img, :password, :password_confirmation)
+      end
+      def account_update_params
+        params.permit(:name, :nickname, :email, :img)
+      end
+    end
+  end
+end
